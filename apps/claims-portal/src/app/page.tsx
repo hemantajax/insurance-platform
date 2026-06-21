d9bac082f9eb765c057c11e26d0b125c35736e00
Hemant <hemant.ajax@gmail.com>
2026-06-21 10:27:25 +0530

    initial commit

---
+++ apps/claims-portal/src/app/page.tsx
@@ -0,0 +1,14 @@
+import styles from './page.module.scss';
+
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.scss file.
+   */
+  return (
+    <div className={styles.page}>
+      <h1>Hello world 2</h1>
+    </div>
+  );
+}
